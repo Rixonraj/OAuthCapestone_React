753ffd84ef8a62722b6568faa13feae573057325
Rixon <rixonraj16@gmail.com>
2023-06-25 00:51:32 +0530

    login h1 value change

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -27,7 +27,7 @@ function Login() {
                     <div className="col-lg-6">
                         <div className="p-5">
                             <div className="text-center">
-                                <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                <h1 className="h4 text-gray-900 mb-4">Welcome To Capestone OAuth!</h1>
                             </div>
                             <form className="user">
                                 <div className="form-group">
